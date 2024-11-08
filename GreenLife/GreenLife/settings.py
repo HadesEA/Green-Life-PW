ed5c57d85bcf3e92fd6f50b1373f2b907db637f2
hades <adesea2209@gmail.com>
2024-11-07 17:23:02 -0800

    creación base de la barra de navaegación para la página de inicio

--- GreenLife/GreenLife/settings.py
+++ GreenLife/GreenLife/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'home',
 ]
 
+THRID_APPS = [
+    'rest_framework',
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
